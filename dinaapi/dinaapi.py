2b04c295755dea84d4a523bfd70873bd62e7a78d
Syed Yead Zaman <s.yead.zaman@gmail.com>
2023-08-01 17:12:01 -0400

    Added temp docs, fixed small issues

--- dinaapi/dinaapi.py
+++ dinaapi/dinaapi.py
@@ -13,22 +13,27 @@ KEYCLOACK_CONFIG_PATH = "./keycloak-config.yml"
 class DinaAPI:
     """Base class containing basic API calls, to be inherited by other classes."""
 
-    def __init__(self, config_path: None) -> None:
+    def __init__(self, config_path: str = None) -> None:
+        """Creates basic web services based on config path provided.
+        Will use a default path if not provided.
+        """
         if config_path == None:
             config_path = KEYCLOACK_CONFIG_PATH
 
-        self.configs = None
-        self.token = None
+        self.configs = None  # contains configuration information
+        self.token = None  # keycloak token
 
         self.set_configs(config_path)
         self.set_token()
 
-        self.req_header = None
+        self.req_header = None  # contains request header for accessing DINA
         self.set_req_header()
 
+        # Base web API URL, to be extended in inherited classes
         self.base_url = "https//dina-dev2.biodiversity.agr.gc.ca/api/"
 
     def set_configs(self, config_path: str) -> None:
+        """Loads config from YAML file and saves it to configs variable."""
         try:
             config_file = open(config_path, "r")
             config_yml = yaml.safe_load(config_file)
@@ -40,6 +45,8 @@ class DinaAPI:
         self.configs = config_yml
 
     def set_token(self) -> None:
+        """Creates a keycloak token based on configs and env variables and
+        saves it to token variable."""
         keycloak_openid = KeycloakOpenID(
             server_url=self.configs["url"],
             client_id=self.configs["client_id"],
@@ -57,6 +64,8 @@ class DinaAPI:
             logging.error(exc)
 
     def set_req_header(self) -> None:
+        """Creates request header based on current token and saves it to
+        req_header variable."""
         self.req_header = {
             "Accept": "application/vnd.api+json",
             "Content-Type": "application/vnd.api+json",
@@ -64,4 +73,6 @@ class DinaAPI:
         }
 
     def get_req_dina(self, full_url: str, params: None):
+        """Base method for a GET request to DINA. The full_url should be
+        an extension of the base_url."""
         return requests.get(full_url, headers=self.req_header, params=params)
--- dinaapi/personapi/personapi.py
+++ dinaapi/personapi/personapi.py
@@ -2,11 +2,15 @@ from ..dinaapi import DinaAPI
 
 
 class PersonAPI(DinaAPI):
-    def __init__(self, config_file: None) -> None:
-        super.__init__(config_file)
+    def __init__(self, config_path: str = None) -> None:
+        """Creates a PersonAPI instance for handling person DINA API requests.
+        Takes an optional path to a config_path"""
+
+        super.__init__(config_path)
         self.base_url += "agent-api/person/"
 
     def find(self, uuid: str):
+        """Returns the GET response of a person with the given UUID."""
         full_url = self.base_url + uuid
 
         return self.get_req_dina(full_url)
