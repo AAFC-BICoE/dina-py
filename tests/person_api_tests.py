6d5a02ce344b488bdc06e8e610b9398127d90827
Syed Yead Zaman <s.yead.zaman@gmail.com>
2023-08-02 15:49:35 -0400

    Added person schema test

--- tests/person_api_tests.py
+++ tests/person_api_tests.py
@@ -1,7 +1,9 @@
 import unittest
-import responses
 
-from dinaapi.personapi.personapi import PersonAPI
+import responses
+from marshmallow.exceptions import ValidationError
+
+from dinaapi.personapi.personapi import PersonAPI, PersonSchema
 
 class PersonAPITestCase(unittest.TestCase):
     @responses.activate
@@ -67,3 +69,40 @@ class PersonAPITestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 404)
         response_data = response.json()
         self.assertEqual(response_data, {"error": "Person not found"})
+
+    def test_person_schema(self):
+        # Sample data for a person
+        person_data = {
+            "id": "bfa3c68b-8e13-4295-8e25-47dbe041cb64",
+            "type": "person",
+            "displayName": "John Doe",
+            "email": "john@example.com",
+            "createdBy": "admin",
+            "createdOn": "2023-08-02T12:00:00Z",
+            "givenNames": "John",
+            "familyNames": "Doe",
+            "webpage": "https://example.com",
+            "remarks": "This is a mock person.",
+            "aliases": ["Johnny"],
+            "organizations": [
+                {"id": "org1", "type": "organization"},
+                {"id": "org2", "type": "organization"},
+            ],
+            "identifiers": [
+                {"id": "identifier1", "type": "identifier"},
+                {"id": "identifier2", "type": "identifier"},
+            ],
+        }
+
+        # Create a PersonSchema instance and validate the sample data
+        person_schema = PersonSchema()
+        try:
+            # Deserialize the data (JSON to Python object)
+            person_object = person_schema.load(person_data)
+            # Serialize the data (Python object to JSON)
+            serialized_data = person_schema.dump(person_object)
+        except ValidationError as exc:
+            self.fail(f"Schema validation failed: {exc}")
+
+        # Compare the serialized data with the original data
+        self.assertDictEqual(serialized_data, person_data)
