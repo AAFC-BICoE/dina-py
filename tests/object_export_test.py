ac81dc86d1f479bd8a24aedd81818f85f4e3e8be
John Phan <johnphan010@gmail.com>
2024-10-03 09:28:11 -0700

    Add getting metadata fileIdentifier to object_export_test.py

--- tests/object_export_test.py
+++ tests/object_export_test.py
@@ -18,27 +18,36 @@ OBJECT_UPLOAD_PATH = "./tests/resources/object-upload-test"
 
 def main():
     dina_api_client = DinaApiClient()
-    pathlist = Path(OBJECT_UPLOAD_PATH).rglob("*.*")
-    with open(DINA_API_CONFIG_TEST_PATH, "r", encoding="utf-8") as dina_api_config_file:
-        dina_api_config: dict = yaml.safe_load(dina_api_config_file)
-        group = dina_api_config["group"]
-        object_export_payload = {
-            "data": {
-                "type": "object-export",
-                "attributes": {
-                    "fileIdentifiers": [
-                        "01924e50-a569-7015-972e-2dea18b0da44",
-                    ],
-                    "name": "dina-py export",
-                },
-            }
-        }
-        response = dina_api_client.objectstore_module_api.create_entity(
-            object_export_payload, "object-export"
-        )
-        response_json = response.json()
+    metadatas_response = dina_api_client.objectstore_module_api.get_entity_by_param(
+        {
+            "filter[originalFilename]": "object_upload - Copy.png",
+        },
+        "metadata",
+    )
+    metadatas_response_json = metadatas_response.json()
 
-        print(response_json)
+    # "01924e45-e991-7800-bce5-c449d05dd18d"
+    metadata_fileIdentifier = metadatas_response_json["data"][0]["attributes"][
+        "fileIdentifier"
+    ]
+
+    object_export_payload = {
+        "data": {
+            "type": "object-export",
+            "attributes": {
+                "fileIdentifiers": [
+                    metadata_fileIdentifier,
+                ],
+                "name": "dina-py export",
+            },
+        }
+    }
+    response = dina_api_client.objectstore_module_api.create_entity(
+        object_export_payload, "object-export"
+    )
+    response_json = response.json()
+
+    print(response_json)
 
 
 if __name__ == "__main__":
