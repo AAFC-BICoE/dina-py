205324f6094eea9f7509f78cdd50aa84b57ec1ac
Robert Gabriel Macalintal <robertgabriel.macalintal@agr.gc.ca>
2024-11-25 10:44:46 -0500

    Added Molecular Analysis Result API, Entity, and Schema

---
+++ dinapy/entities/MolecularAnalysisResult.py
@@ -0,0 +1,69 @@
+class MolecularAnalysisResultDTO:
+	def __init__(self, id=None, type=None, attributes=None, relationships=None):
+		self.id = id
+		self.type = type
+		self.attributes = attributes
+		self.relationships = relationships
+
+	def get_id(self):
+		return self.id
+
+	def get_type(self):
+		return self.type
+
+class MolecularAnalysisResultDTOBuilder:
+	def __init__(self):
+		self._id = None
+		self._type = 'molecular-analysis-result'
+		self._attributes = None
+		self._relationships = None
+
+	def set_id(self, id):
+		self._id = id
+		return self
+
+	def set_type(self, type):
+		self._type = type
+		return self
+
+	def set_attributes(self, attributes):
+		self._attributes = attributes
+		return self
+
+	def set_relationships(self, relationships):
+		self._relationships = relationships
+		return self
+
+	def build(self):
+		return MolecularAnalysisResultDTO(self._id, self._type, self._attributes, self._relationships)
+
+class MolecularAnalysisResultAttributesDTO:
+	def __init__(self, createdBy=None, createdOn=None, group=None):
+		self.createdBy = createdBy
+		self.createdOn = createdOn
+		self.group = group
+	
+class MolecularAnalysisResultAttributesDTOBuilder:
+	def __init__(self):
+		self._group = 'undefined'
+		self._createdBy = 'undefined'
+		self._createdOn = 'undefined'
+	
+	def set_group(self, group):
+		self._group = group
+		return self
+	
+	def set_createdBy(self, createdBy):
+		self._createdBy = createdBy
+		return self
+
+	def set_createdOn(self, createdOn):
+		self._createdOn = createdOn
+		return self
+	
+	def build(self):
+		return MolecularAnalysisResultAttributesDTO(
+			self._group,
+			self._createdBy,
+			self._createdOn
+		)
---
+++ dinapy/schemas/molecularanalysisresultschema.py
@@ -0,0 +1,43 @@
+# This file holds schemas for serializing and deserializing MolecularAnalysisResult entities
+# using the JSON API format. It utilizes the marshmallow_jsonapi library.
+from marshmallow_jsonapi import Schema, fields
+from marshmallow import post_load,pre_load,post_dump,ValidationError
+
+from dinapy.entities.MolecularAnalysisResult import MolecularAnalysisResultDTO
+from .customFields import SkipUndefinedField
+from .BaseSchema import *
+
+class Attachment(BaseSchema):
+    class Meta:
+        type_ = 'attacment'
+
+class MolecularAnalysisRunSchema(Schema):
+    id = fields.Str(load_only=True)
+    group = SkipUndefinedField(fields.Str, required=True, attribute="attributes.group")
+    createdBy = SkipUndefinedField(fields.Str, load_only=True, attribute="attributes.createdBy")
+    createdOn = SkipUndefinedField(fields.Str, load_only=True, attribute="attributes.createdOn")
+
+    attachment = create_relationship("molecular-analysis-result","attachment")
+
+    @post_load
+    def set_none_to_undefined(self, data, **kwargs):
+        for attr in data.attributes:
+            if data.attributes[attr] is None:
+                data.attributes[attr] = 'undefined'
+        return data
+	
+    @post_dump
+    def remove_skipped_fields(self, data, many, **kwargs):
+        # Remove fields with the special marker value
+        return {key: value for key, value in data.items() if value is not SkipUndefinedField(fields.Field).SKIP_MARKER}
+
+    @post_load
+    def object_deserialization(self, data, **kwargs):
+        if 'meta' in data:
+            del data['meta']
+        return MolecularAnalysisResultDTO(**data)
+	
+    meta = fields.DocumentMeta()
+
+    class Meta:
+        type_ = 'molecular-analysis-result'
