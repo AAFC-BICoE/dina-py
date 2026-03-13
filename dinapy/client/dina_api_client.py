b4a886d14a9487ebcec54bf89c8c83efac3ee84c
adamzhou03 <adamzhou03@users.noreply.github.com>
2026-03-13 16:45:44 -0400

    updated upload_file to print filename, uuid, and warnings

--- dinapy/client/dina_api_client.py
+++ dinapy/client/dina_api_client.py
@@ -82,10 +82,11 @@ def upload_file(
     args: argparse.Namespace, dina_api_client: DinaApiClient, path: Path, group
 ):
     response_json: dict = dina_api_client.objectstore_module_api.upload(group, path.as_posix())
+    data = response_json.get('data', {})
     log_response: dict = {
-        "originalFilename": response_json.get("originalFilename"),
-        "uuid": response_json.get("uuid"),
-        "warnings": response_json.get("meta").get("warnings"),
+        "originalFilename": data.get("attributes", {}).get("originalFilename"),
+        "uuid": data.get("id"),
+        "warnings": data.get("meta", {}).get("warnings"),
     }
     print(log_response)
 
@@ -175,12 +176,12 @@ def main():
         path = Path(args.upload_file)
         upload_file(args, dina_api_client, path, group)
     elif args.upload_dir:
-        # .rglob recognizes file patterns
-        pathlist = Path(args.upload_dir).rglob("*.*")
-        for path in pathlist:
-            upload_file(args, dina_api_client, path)
+        for entry in os.listdir(Path(args.upload_dir)):
+            entry_path = Path(args.upload_dir) / entry
+            if entry_path.is_file():
+                upload_file(args, dina_api_client, entry_path, group)
     elif args.create_metadatas:
-        pathlist = Path(args.create_metadatas).rglob("*.*")
+        pathlist = Path(args.create_metadatas)
         dina_api_client.objectstore_module_api.create_metadatas(pathlist, dina_api_config, group)
     elif args.create_form_template:
         path = Path(args.create_form_template)
