4613de5f7d3a82b6746cb41ea40de77283e584c8
Henrique Fonseca <daileonn@gmail.com>
2026-04-22 10:56:14 -0400

    removed marshmallow dependencies and schemas/entities

--- dinapy/schemas/multilingualDescription.py
+++
@@ -1,8 +0,0 @@
-from marshmallow import Schema, fields
-
-class DescriptionSchema(Schema):
-    lang = fields.String(required=True)
-    desc = fields.String(required=True)
-
-class MultilingualDescriptionSchema(Schema):
-    descriptions = fields.List(fields.Nested(DescriptionSchema), allow_none=True)
