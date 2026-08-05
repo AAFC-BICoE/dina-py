659c34ac09be5d95f9cd48e594513e50e544b18d
Henrique Fonseca <daileonn@gmail.com>
2026-08-05 08:36:11 -0400

    38646
    added controlled-vocab-item api

---
+++ dinapy/apis/collectionapi/controlledvocabularyapi.py
@@ -0,0 +1,63 @@
+"""Class that extracts common functionality for controlled vocabulary entity"""
+import json
+import logging
+from .collectionapi import CollectionModuleApi
+
+class ControlledVocabularyItemAPI(CollectionModuleApi):
+
+	def __init__(self, base_url: str = None) -> None:
+		"""
+		Parameters:
+			base_url (str, optional): URL to the URL to perform the API requests against. If not
+				provided then local deployment URL is used. Should end with a forward slash.
+		"""
+		super().__init__( base_url)
+		self.base_url += "controlled-vocabulary-item"
+	
+	def create_entity(self, json_data):
+		"""Creates a DINA controlled vocabulary entity
+
+		Args:
+				json_data (json object): the request body
+
+		Returns:
+				Response: The response post request
+		"""
+		return self.post_req_dina(self.base_url, json_data)
+	
+	def get_entity_by_field(self, field, value):
+		"""Retrieves a DINA controlled vocabulary entity by it's field
+
+		Args:
+			value (string): value of the field eg: 'aafc'
+			field (string): name of the field eg: 'name', 'group'
+
+		Returns:
+			json response: a list of found entities with that value for that field
+		"""
+
+		new_params = {f"filter[{field}][EQ]": value}
+		
+		return self.get_entity_by_param(new_params)
+	
+	def update_entity(self, json_data):
+		return self.patch_req_dina(self.base_url+"/"+f'{json_data["data"]["id"]}', json.dumps(json_data))
+
+	def bulk_update(self, json_data: dict) -> dict:
+		"""Updates controlled-vocabulary records providing a bulk payload using a PATCH request.
+
+		Parameters:
+			json_data (dict): JSON data for updating the controlled-vocabulary.
+
+		Returns:
+			response_data: json content of the response
+		"""
+		full_url = self.base_url
+		
+		try:
+			response_data = self.bulk_update_req_dina(full_url, json_data)
+		except Exception as exc:
+			logging.error(f"Failed to perform bulk update: {exc}")
+			raise  # Re-raise the exception
+
+		return response_data.json()
